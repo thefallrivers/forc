7ee42da784e289c15244c2d65b466c1dbf65677f
thefallrivers <stockmeor@gmail.com>
2024-08-04 22:03:07 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://frosti.saroprock.com',
+  site: 'https://forc.root.sx/',
   output: 'static',
   adapter: vercel({
     webAnalytics: { enabled: true }
